884fa62005120e0aef8106b168fef782fd312021
Spencer Alger <spencer@spenceralger.com>
2014-01-29 18:24:06 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+
+/**
+ * Module dependencies.
+ */
+
+var crypto = require('crypto');
+var _ = require('lodash');
+var async = require('async');
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var auth = require('./lib/auth_mw');
+var LruSessionStore = require('./lib/lru_session');
+
+_.assign(process.env, require('./env'));
+
+var secrets = {
+  cookie: null,
+  session: null
+};
+
+function makeupSecret(name, done) {
+  crypto.randomBytes(256, function (err, buf) {
+    if (err) return done(err);
+    secrets[name] = '' + buf;
+    done();
+  });
+}
+
+function startServer(done) {
+  var app = express();
+
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/lib/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser(secrets.cookie));
+  app.use(express.session({
+    secret: secrets.session,
+    key: 'sid',
+    store: new LruSessionStore()
+  }));
+  app.use(auth.middleware(app));
+  app.use(app.router);
+  app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // development only
+  if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  fs.readdirSync(__dirname + '/lib/routes').forEach(function (file) {
+    require(__dirname + '/lib/routes/' + file)(app);
+  });
+
+  http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+    done();
+  });
+}
+
+async.series([
+  async.apply(async.parallel, Object.keys(secrets).map(function (name) {
+    return makeupSecret.bind(null, name);
+  })),
+  startServer
+]);
