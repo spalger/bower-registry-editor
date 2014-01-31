102e58b73d4ed2c3e8ca5f0a4a7f705e1eedd725
Spencer Alger <spencer@spenceralger.com>
2014-01-31 14:35:47 -0700

    Sessions are now persisted with leveldb, repos with periods will now work, and got rid of everyauth

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var fs = require('fs');
-var auth = require('./lib/auth/middleware');
-var LruSessionStore = require('./lib/lru_session');
+var LeveldbStore = require('connect-leveldb')(express);
+var oneDay = 1000 * 60 * 60 * 24;
 
 var secrets = require('./lib/secrets');
 
@@ -30,6 +30,11 @@ function startServer(done) {
   app.set('trust proxy', !!process.env.NGINX_PROXY);
   app.use(express.favicon());
   app.use(express.logger('dev'));
+
+  if (app.get('env') === 'development') {
+    app.use(express.static(path.join(__dirname, 'public')));
+  }
+
   app.use(express.urlencoded());
   app.use(express.json());
   app.use(express.methodOverride());
@@ -37,8 +42,12 @@ function startServer(done) {
   app.use(express.session({
     secret: secrets.session,
     key: 'sid',
-    store: new LruSessionStore()
+    store: new LeveldbStore({
+      dbLocation: __dirname + '/session.db',
+      ttl: oneDay
+    })
   }));
+
   app.use((function () {
     function sendAsText() {
       this.set('Content-Type', 'text/plain');
@@ -47,17 +56,23 @@ function startServer(done) {
 
     return function (req, res, next) {
       res.text = sendAsText;
+
+      if (req.session && req.session.user) {
+        req.loggedIn = true;
+        res.locals.user = req.user = req.session.user;
+      } else {
+        req.loggedIn = false;
+      }
+
       process.nextTick(next);
     };
   })());
 
-  app.use(auth.root(app));
   app.use(app.router);
   app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 
   // development only
   if ('development' === app.get('env')) {
-    app.use(express.static(path.join(__dirname, 'public')));
     app.use(express.errorHandler());
   } else {
     /* jshint unused: false */
